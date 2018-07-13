73f3ac37bdc8ca3062faaa568b91ec1112b02ec4
PierreRAFFA <pierre.raffa@gmail.com>
2018-07-13 14:11:56 +0100

    Increment App version to 0.4.0

--- app.js
+++ app.js
@@ -112,7 +112,7 @@ const authenticateUser = (req, res, next) => {
  */
 app.get('/v:version/app/settings', function(req, res) {
   const major = 0;
-  const minor = 3;
+  const minor = 4;
   const patch = 0;
   const version = major + '.' + minor + '.' + patch;
   const store = {
